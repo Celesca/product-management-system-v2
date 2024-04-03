923f68890712b1f9c3fe62581de8c0238a62d541
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-04-04 02:54:24 +0700

    🚀  feat: Data Mocking

--- db/initdb.js
+++ db/initdb.js
@@ -3,11 +3,7 @@ const mongoose = require('mongoose');
 const url = 'mongodb://localhost:27017/productDB';
 
 const connectDb = () => {
-    mongoose.connect(url, 
-        {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-          })
+    mongoose.connect(url)
             .then(() => {
               console.log('Connected to MongoDB');
             })
--- db/mockdata.js
+++ db/mockdata.js
@@ -1,12 +1,22 @@
 const Product = require('../models/product');
 
 async function mockData() {
-    const products = [
-      { id: 1, name: 'Laptop', category: 'Electronics', price: 1000, stock: 5},
-      { id: 2, name: 'Typescript Fundamental', category: 'Books', price: 150, stock: 2},
-      { id: 3, name: 'Clean code', category: 'Books', price: 1500, stock: 1}
-    ]
-    await Product.insertMany(products);
-  }
+    try {
+        await Product.deleteMany({});
+
+        const products = [
+            { id: 1, name: 'Laptop', category: 'Electronics', price: 1000, stock: 5 },
+            { id: 2, name: 'Typescript Fundamental', category: 'Books', price: 150, stock: 2 },
+            { id: 3, name: 'Clean code', category: 'Books', price: 1500, stock: 1 }
+        ];
+
+        // Insert new mock data
+        await Product.insertMany(products);
+        
+        console.log('Mock data inserted successfully.');
+    } catch (error) {
+        console.error('Error inserting mock data:', error);
+    }
+}
 
 module.exports = mockData;
