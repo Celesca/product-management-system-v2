7b296d60f12f3b5b12ca3db3bb39bda7e7fbbc49
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-04-05 23:38:59 +0700

    add: mocking mongodb tests

--- tests/server.test.js
+++ tests/server.test.js
@@ -12,10 +12,14 @@ const products = [
 ];
 
 describe('Test request with mongoose', () => {
+
+    beforeEach(async () => {
+        await insertData(products);
+    });
+
     beforeAll(async () => {
         await connect();
         await clearDatabase();
-        await insertData(products);
     });
 
     afterEach(async () => {
@@ -26,66 +30,38 @@ describe('Test request with mongoose', () => {
         await closeDatabase();
     });
 
-    test("should return all products", async() => {
+    // GET /products
+    test("GET /products should return all products", async() => {
         const res = await request(app).get("/products");
         expect(res.statusCode).toBe(200);
     });
 
-    test("should return a single product", async() => {
+    test("GET /product/:id should return a single product", async() => {
         const res = await request(app).get("/products/1");
         expect(res.statusCode).toBe(200);
         expect(res.body._id).toBe(1);
     })
 
-});
-
-
-
-// GET Method
-
-// describe('GET /products', () => {
-//     it("should return all products", async() => {
-//         const res = await request(app).get("/products");
-//         expect(res.statusCode).toBe(200);
-//     })
-// })
-
-describe('GET /products/:id normal', () => {
-    it("should return a single product", async() => {
-        const res = await request(app).get("/products/1");
-        expect(res.statusCode).toBe(200);
-        expect(res.body._id).toBe(1);
-    })
-})
-
-describe('GET /products/:id with no product in products', () => {
-    it("should return a 404", async() => {
+    test("GET /product/:id with no product in database should return a 404", async() => {
         const res = await request(app).get("/products/100");
         expect(res.statusCode).toBe(404);
         expect(res.body.message).toStrictEqual("Product not found");
     })
-})
 
-describe('GET /products/:id with invalid id', () => {
-    it("should return a 400", async() => {
+    it("GET /products/:id with invalid id should return a 400", async() => {
         const res = await request(app).get("/products/-1");
         expect(res.statusCode).toBe(400);
         expect(res.body.message).toStrictEqual("Product ID must be greater than 0");
     })
-})
 
-describe('GET /products/:id with string id', () => {
-    it("should return a 400", async() => {
+    it("GET /products/:id with string id should return a 400", async() => {
         const res =await request(app).get("/products/helloAA1");
         expect(res.statusCode).toBe(400);
         expect(res.body.message).toStrictEqual("Invalid Product ID");
     })
-})
 
-// POST /products
-
-describe('POST /products normal', () => {
-    it("should create a new product", async() => {
+    // POST /products
+    it("POST /product normal should create a new product", async() => {
         const res = await request(app).post("/products").send({
             name: "C++",
             category: "Books",
@@ -93,15 +69,14 @@ describe('POST /products normal', () => {
             stock: 1
         });
         expect(res.statusCode).toBe(200);
+        expect(res.body._id).toBe(4);
         expect(res.body.name).toStrictEqual("C++");
         expect(res.body.category).toStrictEqual("Books");
         expect(res.body.price).toBe(350);
         expect(res.body.stock).toBe(1);
     })
-})
 
-describe('POST /products Invalid Request', () => {
-    it("should return a 400", async() => {
+    it("POST /products Invalid Request should return a 400", async() => {
         const res = await request(app).post("/products").send({
             name: "New Product",
             category: "New Category",
@@ -110,10 +85,8 @@ describe('POST /products Invalid Request', () => {
         expect(res.statusCode).toBe(400);
         expect(res.body.message).toStrictEqual("Invalid request body");
     })
-});
 
-describe('POST /products String Stock', () => {
-    it("should return a 400", async() => {
+    it("POST /products String Stock should return a 400", async() => {
         const res = await request(app).post("/products").send({
             name: "New Product",
             category: "New Category",
@@ -123,10 +96,8 @@ describe('POST /products String Stock', () => {
         expect(res.statusCode).toBe(400);
         expect(res.body.message).toStrictEqual("Invalid request body");
     })
-});
 
-describe('POST /products Minus price', () => {
-    it("should return a 400", async() => {
+    it("POST /products Minus price should return a 400", async() => {
         const res = await request(app).post("/products").send({
             name: "New Product",
             category: "New Category",
@@ -136,10 +107,8 @@ describe('POST /products Minus price', () => {
         expect(res.statusCode).toBe(400);
         expect(res.body.message).toStrictEqual("Price and Stock must be greater than 0");
     })
-});
 
-describe('POST /products Minus stock', () => {
-    it("should return a 400", async() => {
+    it("POST /products Minus stock should return a 400", async() => {
         const res = await request(app).post("/products").send({
             name: "New Product",
             category: "New Category",
@@ -149,12 +118,9 @@ describe('POST /products Minus stock', () => {
         expect(res.statusCode).toBe(400);
         expect(res.body.message).toStrictEqual("Price and Stock must be greater than 0");
     })
-});
 
-// PUT /products/:id
-
-describe('PUT /products/:id normal', () => {
-    it("should update a product", async() => {
+    // PUT /products/:id
+    it("PUT /products/:id normal should update a product", async() => {
         const res = await request(app).put("/products/1").send({
             name: "Updated Product",
             category: "Updated Category",
@@ -171,10 +137,8 @@ describe('PUT /products/:id normal', () => {
         expect(res.statusCode).toBe(200);
         expect(res.body).toStrictEqual(updatedProduct);
     })
-});
 
-describe('PUT /products/:id with no id in products', () => {
-    it("should return a 404", async() => {
+    it("PUT /products/:id with no id in products should return a 404", async() => {
         const res = await request(app).put("/products/100").send({
             name: "Updated Product",
             category: "Updated Category",
@@ -184,10 +148,8 @@ describe('PUT /products/:id with no id in products', () => {
         expect(res.statusCode).toBe(404);
         expect(res.body.message).toStrictEqual("Product not found");
     })
-})
 
-describe('PUT /products/:id with invalid id', () => {
-    it("should return a 400", async() => {
+    it("PUT /products/:id with invalid id should return a 400", async() => {
         const res = await request(app).put("/products/-1").send({
             name: "Updated Product",
             category: "Updated Category",
@@ -197,10 +159,8 @@ describe('PUT /products/:id with invalid id', () => {
         expect(res.statusCode).toBe(400);
         expect(res.body.message).toStrictEqual("Invalid Product ID");
     })
-})
 
-describe('PUT /products/:id Invalid Request', () => {
-    it("should return a 400", async() => {
+    it("PUT /products/:id Invalid Request should return a 400", async() => {
         const res = await request(app).put("/products/1").send({
             name: "Updated Product",
             category: "Updated Category",
@@ -209,10 +169,8 @@ describe('PUT /products/:id Invalid Request', () => {
         expect(res.statusCode).toBe(400);
         expect(res.body.message).toStrictEqual("Invalid request body");
     })
-});
 
-describe('PUT /products/:id string stock', () => {
-    it("should return a 400", async() => {
+    it("PUT /products/:id string stock should return a 400", async() => {
         const res = await request(app).put("/products/1").send({
             name: "Updated Product",
             category: "Updated Category",
@@ -222,10 +180,8 @@ describe('PUT /products/:id string stock', () => {
         expect(res.statusCode).toBe(400);
         expect(res.body.message).toStrictEqual("Invalid request body");
     })
-});
 
-describe('PUT /products/:id Minus stock', () => {
-    it("should return a 400", async() => {
+    it("PUT /products/:id Minus stock should return a 400", async() => {
         const res = await request(app).put("/products/1").send({
             name: "Updated Product",
             category: "Updated Category",
@@ -235,37 +191,29 @@ describe('PUT /products/:id Minus stock', () => {
         expect(res.statusCode).toBe(400);
         expect(res.body.message).toStrictEqual("Price and Stock must be greater than 0");
     })
-});
-
-// DELETE /products/:id
-describe('DELETE /products/:id normal', () => {
-    it("should return a 200 and deleted product", async() => {
+    // DELETE /products/:id
+    it("DELETE /products/:id normal should return a 200 and deleted product", async() => {
         const res = (await request(app).delete("/products/1"));
         expect(res.statusCode).toBe(200);
         expect(res.body).toStrictEqual({message: "Product deleted"});
     })
-})
 
-describe('DELETE /products/:id with not found id', () => {
-    it("should return a 404", async() => {
+    it("DELETE /products/:id with not found id should return a 404", async() => {
         const res = await request(app).delete("/products/25");
         expect(res.statusCode).toBe(404);
         expect(res.body.message).toStrictEqual("Product not found");
     })
-})
 
-describe('DELETE /products/:id with invalid id', () => {
-    it("should return a 400", async() => {
+    it("DELETE /products/:id with invalid id should return a 400", async() => {
         const res = await request(app).delete("/products/-1");
         expect(res.statusCode).toBe(400);
-        expect(res.body.message).toStrictEqual("Invalid ID");
+        expect(res.body.message).toStrictEqual("Invalid Product ID");
     })
-});
 
-describe('DELETE /products/:id with string id', () => {
-    it("should return a 400", async() => {
+    it("DELETE /products/:id with string id should return a 400", async() => {
         const res = await request(app).delete("/products/hello");
         expect(res.statusCode).toBe(400);
-        expect(res.body.message).toStrictEqual("Invalid ID");
+        expect(res.body.message).toStrictEqual("Invalid Product ID");
     })
-})
+
+});
